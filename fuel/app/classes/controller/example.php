60c5d6e0aecc6b38fedd14d3caf9cf1244f87038
sakuriver <suzuerain@yahoo.co.jp>
2020-07-21 12:23:51 +0900

    コントローラからパラメータを渡すサンプルとして用意

--- fuel/app/classes/controller/example.php
+++ fuel/app/classes/controller/example.php
@@ -5,14 +5,28 @@ use Fuel\Core\View;
 
 class Controller_Example extends Controller
 {
-    public function action_test()
+    public function action_test($player_id)
     {
         $data = array();
+        $player_infos = array(
+            "1" => array(
+                "name" => 'Sakuriver',
+                "max_stamina" => 140,
+                "stamina" => 70,
+                "lv" => 18,
+            ),
+            "2" => array(
+                "name" => 'Kamedot',
+                "max_stamina" => 100,
+                "stamina" => 68,
+                "lv" => 25,
+            )
+        );
         $data['title'] = 'River Vs MonsterEnemy';
-        $data['username'] = 'Sakuriver';
-        $data['max_stamina'] = 140;
-        $data['stamina'] = 45;
-        $data['player_lv'] = 18;
+        $data['username'] = $player_infos[$player_id]["name"];
+        $data['max_stamina'] = $player_infos[$player_id]["max_stamina"];
+        $data['stamina'] = $player_infos[$player_id]["stamina"];
+        $data['player_lv'] = $player_infos[$player_id]["lv"];
         $data['monsters'] = array(
             [],
             [],
